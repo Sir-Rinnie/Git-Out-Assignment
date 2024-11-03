d37a3222a48c91de3964f53fb0a9afc1377fc3a4
Sir-Rinnie <hopebagel819@gmail.com>
2024-11-02 21:12:15 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,6 +14,26 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+{
+    if (env.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Home/Error");
+        app.UseHsts();
+    }
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+    app.UseRouting();
+    app.UseAuthorization();
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllers();
+    });
+
 app.UseHttpsRedirection();
 
 var summaries = new[]
